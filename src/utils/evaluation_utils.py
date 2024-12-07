b6ff0fbca413c81dc4190b4cfee94483ea043aad
Edouard <edouard.waked@epfl.ch>
2024-12-07 22:23:56 +0100

    Arranged the financial features

--- src/utils/evaluation_utils.py
+++ src/utils/evaluation_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import math
+from sklearn.preprocessing import MinMaxScaler
 
 def calculate_roi(df, budget_col, revenue_col):
     condition = (df[budget_col].notna() & df[budget_col].ne(0) &
@@ -10,22 +11,20 @@ def calculate_roi(df, budget_col, revenue_col):
     return df_filtered
 
 def scale_features(df):
-    scaler = StandardScaler()
-    minmax_scaler = MinMaxScaler()
-
+    # Scale the ratings between 0 and 1
     df["Normalized_Rating"] = df["Average_ratings"].astype(float) / 10
 
-    # Standardize the selected data
-    df["ROI"] = scaler.fit_transform(df[["ROI"]])
+    # Calculate the Log of ROI to obtain normalized data distribution
+    df["Log_ROI"] = df["ROI"].apply(math.log1p)
 
-    # Apply Min-Max scaling to the already standardized column
-    df["ROI"] = minmax_scaler.fit_transform(df[["ROI"]])
+    # Scale the logged ROI between 0 and 1
+    df["Log_ROI"] = MinMaxScaler().fit_transform(df[["Log_ROI"]])
 
     return df
 
 def calculate_weighted_success(df, roi_weight):
-    df["movie_success"] = df["ROI"] * roi_weight + df["Normalized_Rating"] * (1 - roi_weight)
-    df["movie_success"] = MinMaxScaler().fit_transform(df[["movie_success"]])
+    df["Movie_success"] = df["Log_ROI"] * roi_weight + df["Normalized_Rating"] * (1 - roi_weight)
+    df["Movie_success"] = MinMaxScaler().fit_transform(df[["Movie_success"]])
 
     return df
 
